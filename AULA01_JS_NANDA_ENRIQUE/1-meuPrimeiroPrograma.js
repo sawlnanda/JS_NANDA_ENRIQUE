46f8ffcdfd7a2f144c33ed1b1c92fcde5c7256af
sawlnanda <ferstival1401@gmail.com>
2024-09-06 09:00:09 -0300

    Add files via upload

---
+++ AULA01_JS_NANDA_ENRIQUE/1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+//primeira instrução em js
+console.log("\nOlá mundo cruel da programação\n");
+
+const aluno1="Nanda";
+const aluno2="Enrique";
+
+const idadeAluno1 = 16
+const idadeAluno2 = 15
+
+const somaIdades = idadeAluno1 + idadeAluno2
+
+const dupla = aluno1+" e "+aluno2
+
+console.log("Somos a dupla: "+dupla+"\n")
+console.log("A soma de nossas idades é:" +somaIdades+" anos")
